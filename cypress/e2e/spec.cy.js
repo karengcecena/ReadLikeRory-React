f261c728889fb99784875e12a9a158512503ab74
karengcecena <karengcecena@gmail.com>
2023-05-19 14:11:03 -0700

    Added Basic Testing on User Profile for Features

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,53 @@
+describe('User Profile', () => {
+  beforeEach(() => {
+    cy.visit('/');
+
+    cy.get(".welcome")
+    .find("a")
+    .eq(1)
+    .click();
+
+    cy.get(".child").as("loginForm");
+    
+    cy.get("@loginForm")
+      .find("form")
+      .find("input")
+      .eq(0)
+      .type("karenc");
+      
+    cy.get("@loginForm")
+      .find("input")
+      .eq(1)
+      .type("test1");
+
+    cy.get("@loginForm")
+      .find(".input_btn")
+      .click(); 
+  });
+
+  it('Should Display usernames reading progress', () => {
+    cy.contains('.title h1', "reading progress").should('exist');
+  });
+
+  it('Should Display To Be Read List', () => {
+    cy.get('.alt_child h2')
+    .first()
+    .should('contain', 'To Be Read:');
+  });
+
+  it('Should Display Read List', () => {
+    cy.get('.alt_child h2')
+    .last()
+    .should('contain', 'Read:');
+  });
+
+  it('Should Display Percent Bar', () => {
+    cy.get('.title')
+      .find("progress")
+      .should('be.visible');
+  });
+
+  it('Should Display Percent', () => {
+    cy.get('.title .row').should('contain', '%');
+  });
+});
